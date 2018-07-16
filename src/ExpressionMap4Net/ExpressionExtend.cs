31249f86d03cd61f7a6e65acd61241c3875bcdbd
name <nameemail>
2018-07-16 11:11:27 +0800

    info

---
+++ src/ExpressionMap4Net/ExpressionExtend.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExpressionMap4Net
+{
+    public static class ExpressionExtend
+    {
+        /// <summary>
+        /// 获取lambda表达式的属性或字段名
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public static string GetPropertyOrFieldName(this Expression expression)
+        {
+            if (!(expression is LambdaExpression lambda)) throw new ArgumentException(nameof(expression));
+            if (!(lambda.Body is MemberExpression memberExpr)) throw new ArgumentException(nameof(expression));
+            return memberExpr.Member.Name;
+        }
+    }
+}
